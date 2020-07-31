943245b196decea3b3836843d8dbb16dbc943173
Василий Тунгусов <vtungusov@dgphoenix.com>
2020-07-31 18:50:25 +0700

    changed map key type to more preference

--- src/main/java/vtungusov/parser/StreamParser.java
+++ src/main/java/vtungusov/parser/StreamParser.java
@@ -16,19 +16,19 @@ public class StreamParser implements Parser<Stream<String>> {
 
     @Override
     public Report getSymbolFrequencyReport(Stream<String> stringStream) {
-        Map<String, Integer> frequency = getCharFrequency(stringStream);
+        Map<Character, Integer> frequency = getCharFrequency(stringStream);
         List<String> report = getReportList(frequency);
         return new FrequencyReport(report);
     }
 
     @Override
     public Report getSymbolFrequencyReport(Stream<String> stringStream, int lineCount) {
-        Map<String, Integer> frequency = getCharFrequency(stringStream, lineCount);
+        Map<Character, Integer> frequency = getCharFrequency(stringStream, lineCount);
         List<String> report = getReportList(frequency);
         return new FrequencyReport(report);
     }
 
-    private List<String> getReportList(Map<String, Integer> frequencyMap) {
+    private List<String> getReportList(Map<Character, Integer> frequencyMap) {
         List<String> report = new ArrayList<>();
         int histVertex = Collections.max(frequencyMap.values());
         float histStep = (float) histVertex / HISTOGRAM_SECTION_SIZE;
@@ -41,7 +41,7 @@ public class StreamParser implements Parser<Stream<String>> {
         return report;
     }
 
-    private void formReport(List<String> report, Map<String, Integer> frequencyMap, int totalAmount, float histogramStep, Map.Entry<String, Integer> mapEntry) {
+    private void formReport(List<String> report, Map<Character, Integer> frequencyMap, int totalAmount, float histogramStep, Map.Entry<Character, Integer> mapEntry) {
         BigDecimal percent = BigDecimal.valueOf(frequencyMap.get(mapEntry.getKey()))
                 .multiply(BigDecimal.valueOf(100))
                 .divide(BigDecimal.valueOf(totalAmount), 2, RoundingMode.HALF_EVEN);
@@ -49,7 +49,7 @@ public class StreamParser implements Parser<Stream<String>> {
         report.add(String.format(REPORT_TEMPLATE, mapEntry.getKey(), percent.floatValue(), histogram));
     }
 
-    private String getHistogram(float histStep, Map.Entry<String, Integer> mapEntry) {
+    private String getHistogram(float histStep, Map.Entry<Character, Integer> mapEntry) {
         float stepCount;
         if (histStep != 0) {
             stepCount = mapEntry.getValue() / histStep;
@@ -67,18 +67,18 @@ public class StreamParser implements Parser<Stream<String>> {
         return sb.toString();
     }
 
-    private Map<String, Integer> getCharFrequency(Stream<String> stringStream) {
-        Map<String, Integer> frequency = new HashMap<>();
+    private Map<Character, Integer> getCharFrequency(Stream<String> stringStream) {
+        Map<Character, Integer> frequency = new HashMap<>();
         stringStream
                 .forEach(line -> line.chars()
                         .filter(i -> (!Character.isSpaceChar(i)))
-                        .forEach(charI -> frequency.compute(String.valueOf((char) charI), (k, v) -> (v == null) ? 1 : v + 1)));
+                        .forEach(charI -> frequency.compute((char) charI, (k, v) -> (v == null) ? 1 : v + 1)));
         this.totalSymbols = frequency.values().stream().mapToInt(Integer::intValue).sum();
         return frequency;
     }
 
-    private Map<String, Integer> getCharFrequency(Stream<String> stringStream, int lineCount) {
-        Map<String, Integer> charFrequency = getCharFrequency(stringStream);
+    private Map<Character, Integer> getCharFrequency(Stream<String> stringStream, int lineCount) {
+        Map<Character, Integer> charFrequency = getCharFrequency(stringStream);
         return charFrequency.entrySet()
                 .stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
