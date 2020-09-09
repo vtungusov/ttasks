84d278e296faabb2e6cab7a99a873e1d45281c96
Василий Тунгусов <vtungusov@dgphoenix.com>
2020-09-09 15:25:35 +0700

    cleanup code

--- vocabulary/src/main/java/com/siberteam/vtungusov/vocabulary/handler/Anagramer.java
+++ vocabulary/src/main/java/com/siberteam/vtungusov/vocabulary/handler/Anagramer.java
@@ -9,30 +9,27 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
-import java.util.function.BiFunction;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.siberteam.vtungusov.vocabulary.handler.WordsCollector.WRITE_ERROR;
 
 public class Anagramer {
-    public static final String ANAGRAM_FILE_POSTFIX = "with_anagrams";
-    public static final String EXTENSION_DELIMITER = ".";
+    private static final String ANAGRAM_FILE_POSTFIX = "with_anagrams";
+    private static final String EXTENSION_DELIMITER = ".";
     private static final String POSTFIX_DELIMITER = "_";
-    public static final String ANAGRAM_DELIMITER = ", ";
-    public static final String PREFIX_DELIMITER = ": ";
-    public static final String SUFFIX = "";
-    public static final String INFORMER_DEAD = "Now you're alone, keep on waiting";
-    public static final String ANAGRAMS_SEARCHING = "Started anagrams searching";
-    public static final int MESSAGE_DELAY = 3_000;
-    public static final String INFORM_SYMBOL = ".";
-    public static final String FILE_SAVED = "File was handled and re saved with anagrams as";
+    private static final String ANAGRAM_DELIMITER = ", ";
+    private static final String PREFIX_DELIMITER = ": ";
+    private static final String SUFFIX = "";
+    private static final String FILE_SAVED = "File was handled and re saved with anagrams as";
 
     private final Logger log = LoggerFactory.getLogger(Anagramer.class);
 
     public void findAnagrams(String outputFileName) throws IOException {
-        startInformer();
         FileUtil.checkInputFile(outputFileName);
         final Map<String, List<String>> anagramMap = collectAnagrams(outputFileName);
         final Stream<String> anagrams = getAnagrams(anagramMap);
@@ -41,37 +38,25 @@ public class Anagramer {
 
     private Map<String, List<String>> collectAnagrams(String outputFileName) {
         try (final Stream<String> lines = Files.lines(Paths.get(outputFileName))) {
-            Map<String, List<String>> anagramMap = new HashMap<>();
-            lines.forEach(word -> anagramMap.compute(getSortedWord(word), computeAnagramList(word)));
-            return anagramMap;
+            return lines.collect(Collectors.groupingBy(this::getSortedWord));
         } catch (IOException e) {
             throw new FileIOException(e.getMessage());
         }
-    }
-
-    private BiFunction<String, List<String>, List<String>> computeAnagramList(String word) {
-        return (key, list) -> {
-            List<String> result;
-            if (list == null) {
-                result = Collections.singletonList(word);
-            } else {
-                final ArrayList<String> strings = new ArrayList<>(list);
-                strings.add(word);
-                result = strings;
-            }
-            return result;
-        };
     }
 
     private Stream<String> getAnagrams(Map<String, List<String>> anagrams) {
         return anagrams.values().stream()
                 .filter(list -> list.size() > 1)
                 .flatMap(list -> list.stream()
-                        .map(anagram ->
-                                list.stream()
-                                        .filter(s -> !s.equals(anagram))
-                                        .collect(Collectors.joining(ANAGRAM_DELIMITER, anagram + PREFIX_DELIMITER, SUFFIX)))
+                        .map(getAnagramCombination(list))
                 );
+    }
+
+    private Function<String, String> getAnagramCombination(List<String> list) {
+        return anagram ->
+                list.stream()
+                        .filter(s -> !s.equals(anagram))
+                        .collect(Collectors.joining(ANAGRAM_DELIMITER, anagram + PREFIX_DELIMITER, SUFFIX));
     }
 
     private String getSortedWord(String word) {
@@ -85,7 +70,6 @@ public class Anagramer {
         try {
             FileUtil.checkOutputFile(outputFileName);
             Files.write(Paths.get(outputFileName), (Iterable<String>) strings::iterator);
-            System.out.println();
             log.info("{} {}", FILE_SAVED, outputFileName);
         } catch (IOException e) {
             final String message = e.getMessage();
@@ -106,25 +90,5 @@ public class Anagramer {
             result = base + postfix;
         }
         return result;
-    }
-
-    private void startInformer() {
-        final Thread informer = new Thread(() -> {
-            System.out.print(ANAGRAMS_SEARCHING);
-            System.out.println();
-            while (true) {
-                try {
-                    for (int i = 0; i < 50; i++) {
-                        Thread.sleep(MESSAGE_DELAY);
-                        System.out.print(INFORM_SYMBOL);
-                    }
-                    System.out.println();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(INFORMER_DEAD);
-                }
-            }
-        });
-        informer.setDaemon(true);
-        informer.start();
     }
 }
