265fa67d9e73fcdf5eb1e4858040408a4e536982
Василий Тунгусов <vtungusov@dgphoenix.com>
2020-09-15 19:02:54 +0700

    change logic

--- calculator/src/main/java/com/siberteam/vtungusov/calc/action/CalcAction.java
+++ calculator/src/main/java/com/siberteam/vtungusov/calc/action/CalcAction.java
@@ -1,6 +1,8 @@
 package com.siberteam.vtungusov.calc.action;
 
 import com.siberteam.vtungusov.calc.form.CalcForm;
+import com.siberteam.vtungusov.calc.form.OperationType;
+import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -8,13 +10,13 @@ import org.apache.struts.action.ActionMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class CalcAction extends AbstractCalcAction {
+public class CalcAction extends Action {
 
     @Override
     public ActionForward execute(ActionMapping mapping, ActionForm form,
                                  HttpServletRequest request, HttpServletResponse response) {
         CalcForm calcForm = (CalcForm) form;
-        calculate(calcForm);
+        OperationType.valueOf(calcForm.getAction()).calculate(calcForm);
         return mapping.findForward("success");
     }
 }
--- calculator/src/main/java/com/siberteam/vtungusov/calc/form/OperationType.java
+++ calculator/src/main/java/com/siberteam/vtungusov/calc/form/OperationType.java
@@ -1,10 +1,52 @@
 package com.siberteam.vtungusov.calc.form;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public enum OperationType {
-    ADDITION("+"),
-    SUBTRACTION("-"),
-    MULTIPLICATION("*"),
-    DIVISION("/");
+    ADDITION("+") {
+        @Override
+        protected Double compute(double num1, double num2) {
+            return BigDecimal.valueOf(num1)
+                    .add(BigDecimal.valueOf(num2))
+                    .doubleValue();
+        }
+    },
+    SUBTRACTION("-") {
+        @Override
+        protected Double compute(double num1, double num2) {
+            return BigDecimal.valueOf(num1)
+                    .subtract(BigDecimal.valueOf(num2))
+                    .doubleValue();
+
+        }
+    },
+    MULTIPLICATION("*") {
+        @Override
+        protected Double compute(double num1, double num2) {
+            return BigDecimal.valueOf(num1)
+                    .multiply(BigDecimal.valueOf(num2))
+                    .doubleValue();
+        }
+    },
+    DIVISION("/") {
+        @Override
+        protected Double compute(double num1, double num2) {
+            return BigDecimal.valueOf(num1)
+                    .divide(BigDecimal.valueOf(num2), RESULT_SCALE, RoundingMode.HALF_EVEN)
+                    .doubleValue();
+        }
+    };
+
+    private static final int RESULT_SCALE = 10;
+
+    public void calculate(CalcForm form) {
+        final double operand1 = form.getOperand1();
+        final double operand2 = form.getOperand2();
+        final OperationType action = OperationType.valueOf(form.getAction());
+        Double result = compute(operand1, operand2);
+        form.setResult(String.format("%s %s %s = %s", operand1, action.getValue(), operand2, result));
+    }
 
     private final String value;
 
@@ -15,4 +57,6 @@ public enum OperationType {
     public String getValue() {
         return value;
     }
+
+    protected abstract Double compute(double num1, double num2);
 }
